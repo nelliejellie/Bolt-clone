a373497df63ac6862265dbe320d48fa185a2d3b7
Emeka Ewelike <emekaewelike@gmail.com>
2022-10-11 21:19:05 +0100

    dotenv added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,8 @@ import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity } from 'r
 import React from 'react'
 import tw from "tailwind-react-native-classnames"
 import NavOptions from '../components/NavOptions'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import {GOOGLE_MAPS_APIKEY} from "@env"
 
 const HomeScreen = ({navigation}) => {
   return (
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,11 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
+import tw from "tailwind-react-native-classnames"
 
 const MapScreen = () => {
   return (
-    <View>
-      <Text>MapScreen</Text>
-    </View>
+    <SafeAreaView style={tw`h-full p-5`}>
+      <View>
+        <Text>MapScreen</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
