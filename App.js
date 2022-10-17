af0fcab99a222beead88833d2d8280ba84ee8514
Emeka Ewelike <emekaewelike@gmail.com>
2022-10-17 11:34:20 +0100

    about to start ride options

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, Platform } from 'react-native';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import {store} from "./store/store"
@@ -8,6 +8,7 @@ import "react-native-gesture-handler";
 import {NavigationContainer} from "@react-navigation/native"
 import { createStackNavigator } from '@react-navigation/stack';
 import MapScreen from './screens/MapScreen';
+import tw from 'tailwind-react-native-classnames';
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -16,18 +17,23 @@ export default function App() {
         <SafeAreaProvider>
           <View style={styles.container}>
           <NavigationContainer>
-            <Stack.Navigator>
-              <Stack.Screen name="HomeScreen" component={HomeScreen}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen name="MapScreen" component={MapScreen} 
-                options={{
-                  headerShown: false,
-                }}
-              />
-            </Stack.Navigator>
+            <KeyboardAvoidingView style={tw`flex-1`}
+              behavior={Platform.OS === "ios" ? "padding" : "height"}
+              keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+            >
+              <Stack.Navigator>
+                <Stack.Screen name="HomeScreen" component={HomeScreen}
+                  options={{
+                    headerShown: false,
+                  }}
+                />
+                <Stack.Screen name="MapScreen" component={MapScreen} 
+                  options={{
+                    headerShown: false,
+                  }}
+                />
+              </Stack.Navigator>
+            </KeyboardAvoidingView>
           </NavigationContainer>
           </View>  
         </SafeAreaProvider>
--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -7,53 +7,61 @@ import { useDispatch } from 'react-redux'
 import { setDestination } from '../store/slices/navSlice'
 import { useNavigation } from '@react-navigation/native'
 import NavFavorites from './NavFavorites'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { Icon } from 'react-native-elements'
 
 const NavigateCard = () => {
   const navigation = useNavigation()
   const dispatch = useDispatch();
   return (
     <SafeAreaView style={tw`bg-white flex-1`}> 
-    <TouchableWithoutFeedback onPress={()=>Keyboard.dismiss()}>
-        <Text style={tw`text-center py-5 text-xl`}>
-            Good morning, Nelson
-        </Text>
-    </TouchableWithoutFeedback>
-    <View style={tw`border-t border-gray-200 flex-shrink`}>
-    <View>
-        <GooglePlacesAutocomplete
-            placeholder='where to'
-            debounce={400}
-            key={GOOGLE_MAPS_APIKEY}
-            nearbyPlacesAPI="GooglePlacesSearch"
-            enablePoweredByContainer={false}
-            fetchDetails={true}
-            onPress={(data, details= null)=>{
-                dispatch(setDestination({
-                    location: details.geometry.location,
-                    description: data.description
-                }))
-                navigation.navigate("RideOptionsCard")
-            }}
-            query={{
-                key: GOOGLE_MAPS_APIKEY,
-                language: "en"
-            }}
-            styles={{
-                container:{
-                    flex:0,
-                    marginLeft:10,
-                    marginRight:10,
-                    backgroundColor:'white'
-                },
-                textInput:{
-                    fontSize: 18,
-                    backgroundColor:'#DDDDDF'
-                },
-            }}
-        />
-    </View>
-    </View>
-    <NavFavorites/>
+        <TouchableWithoutFeedback onPress={()=>Keyboard.dismiss()}>
+            <Text style={tw`text-center py-5 text-xl`}>
+                Good morning, Nelson
+            </Text>
+        </TouchableWithoutFeedback>
+        <View style={tw`border-t border-gray-200 flex-shrink`}>
+            <View>
+                <GooglePlacesAutocomplete
+                    placeholder='where to'
+                    debounce={400}
+                    key={GOOGLE_MAPS_APIKEY}
+                    nearbyPlacesAPI="GooglePlacesSearch"
+                    enablePoweredByContainer={false}
+                    fetchDetails={true}
+                    onPress={(data, details= null)=>{
+                        dispatch(setDestination({
+                            location: details.geometry.location,
+                            description: data.description
+                        }))
+                        navigation.navigate("RideOptionsCard")
+                    }}
+                    query={{
+                        key: GOOGLE_MAPS_APIKEY,
+                        language: "en"
+                    }}
+                    styles={{
+                        container:{
+                            flex:0,
+                            marginLeft:10,
+                            marginRight:10,
+                            backgroundColor:'white'
+                        },
+                        textInput:{
+                            fontSize: 18,
+                            backgroundColor:'#DDDDDF'
+                        },
+                    }}
+                />
+            </View>
+            <NavFavorites/>
+        </View>
+        <View style={tw`flex-1 flex-row justify-center py-4`}>
+            <TouchableOpacity style={tw`flex flex-row justify-around items-center bg-green-300 w-48 px-4 py-3 rounded-full`}>
+                <Icon name="car" type="font-awesome" color="black" sixe={16}/>
+                <Text style={tw`text-black font-bold text-center`}>Rides</Text>
+            </TouchableOpacity>
+        </View>
     </SafeAreaView>
   )
 }
